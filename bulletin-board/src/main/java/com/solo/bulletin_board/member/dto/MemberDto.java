98f37fffaad12192834a7b7ec2353c87c87b7368
0KSY <uryhon7@gmail.com>
2025-08-04 15:03:46 +0900

    #3 : member 기본 동작 정의

--- bulletin-board/src/main/java/com/solo/bulletin_board/member/dto/MemberDto.java
+++ bulletin-board/src/main/java/com/solo/bulletin_board/member/dto/MemberDto.java
@@ -1,4 +1,45 @@
 package com.solo.bulletin_board.member.dto;
 
+import com.solo.bulletin_board.member.entity.Member;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
 public class MemberDto {
+
+    @Getter
+    @Setter
+    public static class Post{
+
+        @NotBlank
+        private String email;
+        @NotBlank
+        private String nickname;
+    }
+
+    @Getter
+    @Setter
+    public static class Patch{
+
+        private long memberId;
+        private String nickname;
+        private Member.MemberStatus memberStatus;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    public static class Response{
+
+        private long memberId;
+        private String email;
+        private String nickname;
+        private Member.MemberStatus memberStatus;
+        private LocalDateTime createdAt;
+        private LocalDateTime modifiedAt;
+
+    }
 }
