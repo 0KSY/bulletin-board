2a1bc527ab75597b2ef525a53109d7bb474b10d0
0KSY <uryhon7@gmail.com>
2025-08-11 13:15:21 +0900

    #10 : memberId 검증 로직 추가

--- bulletin-board/src/main/java/com/solo/bulletin_board/comment/service/CommentService.java
+++ bulletin-board/src/main/java/com/solo/bulletin_board/comment/service/CommentService.java
@@ -1,9 +1,12 @@
 package com.solo.bulletin_board.comment.service;
 
+import com.solo.bulletin_board.auth.userDetailsService.CustomUserDetails;
 import com.solo.bulletin_board.comment.entity.Comment;
 import com.solo.bulletin_board.comment.repository.CommentRepository;
 import com.solo.bulletin_board.exception.BusinessLogicException;
 import com.solo.bulletin_board.exception.ExceptionCode;
+import com.solo.bulletin_board.member.entity.Member;
+import com.solo.bulletin_board.member.service.MemberService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,9 +17,11 @@ import java.util.Optional;
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final MemberService memberService;
 
-    public CommentService(CommentRepository commentRepository) {
+    public CommentService(CommentRepository commentRepository, MemberService memberService) {
         this.commentRepository = commentRepository;
+        this.memberService = memberService;
     }
 
     public Comment findVerifiedComment(long commentId){
@@ -29,13 +34,19 @@ public class CommentService {
 
     }
 
-    public Comment createComment(Comment comment){
+    public Comment createComment(Comment comment, CustomUserDetails customUserDetails){
+
+        Member findMember = memberService.findVerifiedMember(customUserDetails.getMemberId());
+        comment.setMember(findMember);
+
         return commentRepository.save(comment);
     }
 
-    public Comment updateComment(Comment comment){
+    public Comment updateComment(Comment comment, CustomUserDetails customUserDetails){
 
         Comment findComment = findVerifiedComment(comment.getCommentId());
+
+        memberService.checkMemberId(findComment.getMember().getMemberId(), customUserDetails);
 
         Optional.ofNullable(comment.getContent())
                 .ifPresent(content -> findComment.setContent(content));
@@ -44,8 +55,10 @@ public class CommentService {
 
     }
 
-    public void deleteComment(long commentId){
+    public void deleteComment(long commentId, CustomUserDetails customUserDetails){
         Comment findComment = findVerifiedComment(commentId);
+
+        memberService.checkMemberId(findComment.getMember().getMemberId(), customUserDetails);
 
         commentRepository.delete(findComment);
     }
